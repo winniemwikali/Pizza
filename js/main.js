3323dd8d7e468825b5212f1b74ca03cb9344efdc
moringaschool-Winnie <mwikaliwinnie303@gmail.com>
2019-12-12 17:21:20 +0300

    changed scripts.js to main.js

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const order = document.getElementById("btn");
+    order.addEventListener("click", function(){
+        
+        var pSize = parseInt(document.getElementById("pizzaSize").value);
+        var pCrust = parseInt(document.getElementById("pizzaCrust").value);
+        var pToppings = parseInt(document.getElementById("pizzaToppings").value);
+        var quant = parseInt(document.getElementById("quantity").value);
+            
+        var totalCost = (pSize + pCrust + pToppings) * quant;
+
+        alert(`Your order has been received amounting to ${totalCost}`);
+        prompt(`please enter your location.`);
+        alert(`Your order will be delivered in a few, please have some ksh.300 for delivery.`)
+
+        if (delivery.checked) {
+            
+        } else {
+            
+        }
+        prompt(``)
+
+        
+
+    
+    });
+
